20fcff7263c115ca297f3af85b3ccfa188efea3b
haidi20 <haidinurhadinata@gmail.com>
2025-06-24 10:43:11 +0800

    memperbaiki pagination

--- BE/resources/js/components/Paginate.vue
+++ BE/resources/js/components/Paginate.vue
@@ -6,42 +6,70 @@ defineProps({
     required: true,
   },
 });
+
+function onChangePage(page) {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
 </script>
 
 <template>
   <nav aria-label="Page navigation example">
     <ul class="pagination justify-content-center mt-4">
-      <!-- Previous Button -->
+      <!-- Tombol First -->
+      <li class="page-item" :class="{ disabled: pagination.current_page === 1 }">
+        <Link
+          class="page-link"
+          @click="onChangePage(1)"
+          :href="`${pagination.path}?page=1`"
+          preserve-scroll
+        >&laquo; First</Link>
+      </li>
+
+      <!-- Tombol Previous -->
       <li class="page-item" :class="{ disabled: !pagination.links[0].url }">
-        <Link class="page-link" :href="pagination.links[0].url || '#'" preserve-scroll>
-          <span v-html="pagination.links[0].label"></span>
+        <Link
+          class="page-link"
+          @click="onChangePage(pagination.current_page - 1)"
+          :href="pagination.links[0].url || '#'"
+          preserve-scroll
+        >
+          <span>Previous</span>
         </Link>
       </li>
 
-      <!-- Page Numbers -->
+      <!-- Nomor Halaman -->
       <li
         class="page-item"
+        @click="onChangePage(pagination.current_page)"
         :class="{ active: link.active, disabled: !link.url }"
         v-for="(link, index) in pagination.links.slice(1, -1)"
         :key="index"
       >
         <Link class="page-link" :href="link.url || '#'" preserve-scroll>
-          <span v-html="link.label"></span>
+          <span>{{ link.label }}</span>
         </Link>
       </li>
 
-      <!-- Next Button -->
-      <li
-        class="page-item"
-        :class="{ disabled: !pagination.links[pagination.links.length - 1].url }"
-      >
+      <!-- Tombol Next -->
+      <li class="page-item" :class="{ disabled: !pagination.links.at(-1).url }">
         <Link
           class="page-link"
-          :href="pagination.links[pagination.links.length - 1].url || '#'"
+          @click="onChangePage(pagination.current_page + 1)"
+          :href="pagination.links.at(-1).url || '#'"
           preserve-scroll
         >
-          <span v-html="pagination.links[pagination.links.length - 1].label"></span>
+          <span>Next</span>
         </Link>
+      </li>
+
+      <!-- Tombol Last -->
+      <li class="page-item" :class="{ disabled: pagination.current_page === pagination.last_page }">
+        <Link
+          class="page-link"
+          :href="`${pagination.path}?page=${pagination.last_page}`"
+          @click="onChangePage(pagination.last_page)"
+          preserve-scroll
+        >Last &raquo;</Link>
       </li>
     </ul>
   </nav>
